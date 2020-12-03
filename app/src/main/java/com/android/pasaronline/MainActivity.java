b5906ae4e5ce2a5b24dc7ca8fa780ab0796fee48
BayuAries <62095092+BayuAries@users.noreply.github.com>
2020-12-03 11:11:51 +0700

    add

--- app/src/main/java/com/android/pasaronline/MainActivity.java
+++ app/src/main/java/com/android/pasaronline/MainActivity.java
@@ -2,7 +2,12 @@ package com.android.pasaronline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button logout = findViewById(R.id.btnLogOut);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+            }
+        });
     }
 }
